b3f345338898a60c91b2b36491386244659736d0
DEVAANSH001 <theman3185@gmail.com>
2025-07-15 00:00:05 +0530

    updated buffer read

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const ws = require('ws');
-const server = new ws.Server({ port: '3000' });
+const ws = require('ws')
+const server = new ws.Server({ port: '3000' })
 
-server.on('connection', (socket) => {
-    socket.on('message', (message) => {
-        console.log(`Received message: ${message}`);
-        socket.send(`Server received: ${message}`);
-    });
+server.on('connection', socket => {
+    socket.on('message', message => {
+        const b = Buffer.from(message)
+        console.log(b.toString())
+        socket.send(`${message}`)
+    })
 })
